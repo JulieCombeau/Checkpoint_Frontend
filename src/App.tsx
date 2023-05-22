d04c6e5d82c542b3bc97b9e605d2e3071ba7367d
Julie Combeau <combeau.julie@gmail.com>
2023-05-22 14:47:17 +0200

    Initialized routes and apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Continents from "./pages/Continents";
+import Countries from "./pages/Countries";
+import CountryDetails from "./pages/CountryDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Continents />} />
+          <Route path="/:continentName" element={<Countries />} />
+          <Route
+            path="/:continentName/:countryName"
+            element={<CountryDetails />}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
