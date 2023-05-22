81f3e07503621e64a4df36e8b79f8c283296e99b
Julie Combeau <combeau.julie@gmail.com>
2023-05-22 16:13:40 +0200

    ✨ Create page continents and add navigate to one continent page

---
+++ src/gql/generated/schema.ts
@@ -0,0 +1,164 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Upload: any;
+};
+
+export enum CacheControlScope {
+  Private = 'PRIVATE',
+  Public = 'PUBLIC'
+}
+
+export type Continent = {
+  __typename?: 'Continent';
+  code: Scalars['ID'];
+  countries: Array<Country>;
+  name: Scalars['String'];
+};
+
+export type ContinentFilterInput = {
+  code?: InputMaybe<StringQueryOperatorInput>;
+};
+
+export type Country = {
+  __typename?: 'Country';
+  capital?: Maybe<Scalars['String']>;
+  code: Scalars['ID'];
+  continent: Continent;
+  currency?: Maybe<Scalars['String']>;
+  emoji: Scalars['String'];
+  emojiU: Scalars['String'];
+  languages: Array<Language>;
+  name: Scalars['String'];
+  native: Scalars['String'];
+  phone: Scalars['String'];
+  states: Array<State>;
+};
+
+export type CountryFilterInput = {
+  code?: InputMaybe<StringQueryOperatorInput>;
+  continent?: InputMaybe<StringQueryOperatorInput>;
+  currency?: InputMaybe<StringQueryOperatorInput>;
+};
+
+export type Language = {
+  __typename?: 'Language';
+  code: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
+  native?: Maybe<Scalars['String']>;
+  rtl: Scalars['Boolean'];
+};
+
+export type LanguageFilterInput = {
+  code?: InputMaybe<StringQueryOperatorInput>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  continent?: Maybe<Continent>;
+  continents: Array<Continent>;
+  countries: Array<Country>;
+  country?: Maybe<Country>;
+  language?: Maybe<Language>;
+  languages: Array<Language>;
+};
+
+
+export type QueryContinentArgs = {
+  code: Scalars['ID'];
+};
+
+
+export type QueryContinentsArgs = {
+  filter?: InputMaybe<ContinentFilterInput>;
+};
+
+
+export type QueryCountriesArgs = {
+  filter?: InputMaybe<CountryFilterInput>;
+};
+
+
+export type QueryCountryArgs = {
+  code: Scalars['ID'];
+};
+
+
+export type QueryLanguageArgs = {
+  code: Scalars['ID'];
+};
+
+
+export type QueryLanguagesArgs = {
+  filter?: InputMaybe<LanguageFilterInput>;
+};
+
+export type State = {
+  __typename?: 'State';
+  code?: Maybe<Scalars['String']>;
+  country: Country;
+  name: Scalars['String'];
+};
+
+export type StringQueryOperatorInput = {
+  eq?: InputMaybe<Scalars['String']>;
+  glob?: InputMaybe<Scalars['String']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['String']>>>;
+  ne?: InputMaybe<Scalars['String']>;
+  nin?: InputMaybe<Array<InputMaybe<Scalars['String']>>>;
+  regex?: InputMaybe<Scalars['String']>;
+};
+
+export type ContinentsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type ContinentsQuery = { __typename?: 'Query', continents: Array<{ __typename?: 'Continent', code: string, name: string }> };
+
+
+export const ContinentsDocument = gql`
+    query continents {
+  continents {
+    code
+    name
+  }
+}
+    `;
+
+/**
+ * __useContinentsQuery__
+ *
+ * To run a query within a React component, call `useContinentsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useContinentsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useContinentsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useContinentsQuery(baseOptions?: Apollo.QueryHookOptions<ContinentsQuery, ContinentsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<ContinentsQuery, ContinentsQueryVariables>(ContinentsDocument, options);
+      }
+export function useContinentsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ContinentsQuery, ContinentsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<ContinentsQuery, ContinentsQueryVariables>(ContinentsDocument, options);
+        }
+export type ContinentsQueryHookResult = ReturnType<typeof useContinentsQuery>;
+export type ContinentsLazyQueryHookResult = ReturnType<typeof useContinentsLazyQuery>;
+export type ContinentsQueryResult = Apollo.QueryResult<ContinentsQuery, ContinentsQueryVariables>;
--- src/pages/Continents.tsx
+++ src/pages/Continents.tsx
@@ -1,7 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useContinentsQuery } from "../gql/generated/schema";
+import "../styles/continents.css";
+
 export default function Continents() {
+  const navigate = useNavigate();
+  const { data } = useContinentsQuery();
+
+  const continentsList = data?.continents;
+
+  console.log(continentsList);
+
   return (
-    <div>
-      <h2>Continents</h2>
-    </div>
+    <>
+      <div className="continents-container">
+        <h2 className="continents-title">Continents</h2>
+        <div className="continents_list">
+          {continentsList?.map((continent) => (
+            <div
+              className="continents_item"
+              onClick={() => navigate(`/${continent.name}`)}
+            >
+              <p>{continent.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
